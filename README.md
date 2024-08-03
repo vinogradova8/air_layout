debaa40266e25002510165ed233e135e57ce636a
Viktoriia Vynohradova <viktoriia.vynohradova8@gmail.com>
2024-08-03 18:34:23 +0300

    clone air layout

--- README.md
+++ README.md
@@ -1,4 +1,27 @@
-# Base layout template with Gulp, SCSS and Stylelint
-1. Create a repo using this template
-1. Replace `<your_account>` and `<repo_name>` with your Github username and the new repo name
-    - [DEMO LINK](https://<your_account>.github.io/<repo_name>/)
+<h4>This project is the website of the Strategic Agency named Air. The agency helps companies solve business problems. Air is a full service creative agency.</h4>
+
+<h4>There are 5 main sections on the website:</h4>
+<p>Home</p>
+<p>About us</p>
+<p>Work</p>
+<p>Services
+</p>
+<p>Contact us</p>
+
+<h4>Technologies that were used:</h4>
+<p>HTML - was used to build semantically correct pages.</p>
+<p>SCSS - was used for styling.</p>
+<p>JavaScript - helped me to make my website interactive.
+</p>
+<p>Gulp - was used as a streaming build system.</p>
+
+<h4>To run the project locally:</h4>
+
+1. Clone the repo - select Code -> Local -> HTTPS/SSH and copy the link.
+2. Run 'git clone your_link'.
+3. Run 'npm install' to install the dependencies.
+4. Run 'npm start'.
+
+Link to the Figma design - [Air agency](<https://www.figma.com/file/7qwsWggv9BAxMi2VPhBuPr/Air-(formerly-Dia)?node-id=9138%3A35>)
+
+Link to the preview - [DEMO LINK](https://vinogradova8.github.io/air_layout/)
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,90 @@
 'use strict';
+
+const sliderList = document.querySelector('.slider__list');
+const buttonLeft = document.querySelector('.slider-button--left');
+const buttonRight = document.querySelector('.slider-button--right');
+const body = document.querySelector('body');
+const page = document.querySelector('html');
+const form = document.querySelector('form');
+const info = document.querySelector('.header-bottom__info');
+const topBar = document.querySelector('.top-bar');
+const menuOpen = document.querySelector('.icon--menu');
+const menuClose = document.querySelector('.icon--close');
+const menuLink = document.querySelector('.menu-link');
+
+menuOpen.addEventListener('click', () => {
+  page.classList.add('scroll-lock');
+});
+
+menuClose.addEventListener('click', () => {
+  page.classList.remove('scroll-lock');
+});
+
+menuLink.addEventListener('click', () => {
+  page.classList.remove('scroll-lock');
+});
+
+const numberOfSlides = sliderList.childElementCount;
+
+let count = 0;
+
+buttonRight.addEventListener('click', () => {
+  if (count == numberOfSlides - 1) {
+    return;
+  }
+
+  if (count == numberOfSlides - 2) {
+    buttonRight.classList.add('slider-button--disabled');
+  }
+
+  buttonLeft.classList.remove('slider-button--disabled');
+
+  sliderList.style.transform = `translateX(-${(count + 1) * 100}%)`;
+  count++;
+});
+
+if (count === 0) {
+  buttonLeft.classList.add('slider-button--disabled');
+}
+
+buttonLeft.addEventListener('click', () => {
+  if (count === 0) {
+    buttonLeft.classList.add('slider-button--disabled');
+    return;
+  }
+
+  if (count === 1) {
+    buttonLeft.classList.add('slider-button--disabled');
+  }
+
+  buttonRight.classList.remove('slider-button--disabled');
+
+  sliderList.style.transform = `translateX(-${(count - 1) * 100}%)`;
+  count--;
+});
+
+window.addEventListener('load', () => {
+  let documentWidth = body.offsetWidth;
+  if (parseInt(documentWidth) > 1148) {
+    info.style.paddingLeft = `${(parseInt(documentWidth) - 1148) / 2 + 54}px`;
+  }
+  document.documentElement.classList.add('loaded');
+});
+
+window.addEventListener('resize', () => {
+  let documentWidth = body.offsetWidth;
+  if (parseInt(documentWidth) > 1148) {
+    info.style.paddingLeft = `${(parseInt(documentWidth) - 1148) / 2 + 54}px`;
+  }
+
+  if (parseInt(documentWidth) < 1148) {
+    info.style.paddingLeft = topBar.style.paddingLeft;
+  }
+});
+
+form.addEventListener('submit', (event) => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+});
